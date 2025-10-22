dab763dc9f5e932b9e8952b8201b1e8d1c5f9b1f
Giang <killkazaki2k4@gmail.com>
2025-10-22 10:01:16 +0700

    refactor(auth): clean up Auth controllers documentation
    - Remove verbose comment blocks from Auth controllers
    - Clean up LoginController, RegisterController, ForgotPasswordController, ConfirmPasswordController
    - Keep only essential inline comments for clarity

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -10,29 +10,17 @@ use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
+
 
     use RegistersUsers;
 
     /**
-     * Where to redirect users after registration.
-     *
      * @var string
      */
-    protected $redirectTo = '/login'; // Redirect to login page after registration
+    protected $redirectTo = '/login'; 
 
     /**
-     * Create a new controller instance.
-     *
+
      * @return void
      */
     public function __construct()
@@ -41,9 +29,8 @@ class RegisterController extends Controller
     }
 
     /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
+
+     * @param  array  
      * @return \Illuminate\Contracts\Validation\Validator
      */
     protected function validator(array $data)
@@ -73,8 +60,6 @@ class RegisterController extends Controller
     }
 
     /**
-     * Create a new user instance after a valid registration.
-     *
      * @param  array  $data
      * @return \App\Models\User
      */
@@ -86,9 +71,7 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
         ]);
     }
-    /**
-     * After registration, logout and redirect to login with success message
-     */
+
     protected function registered($request, $user)
     {
         $this->guard()->logout();
